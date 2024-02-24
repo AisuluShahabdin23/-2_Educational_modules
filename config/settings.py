594d9295eb477f43208ca342bc881e83fc0e2937
Aisulu Shahabdin <aisulu_sh_98@mail.ru>
2024-02-24 23:55:56 +0600

    change

--- config/settings.py
+++ config/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'PORT': os.getenv('POSTGRES_POST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
         'HOST': os.getenv('POSTGRES_HOST'),
     }
 }
